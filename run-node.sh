b7a400df22c9c150f33ca2a762721dac33325717
Jason Plumb <jplumb@splunk.com>
2022-08-25 13:17:15 -0700

    update script

--- run-node.sh
+++ run-node.sh
@@ -1,8 +1,8 @@
-export REALM="lab0"
+export REALM="us1"
 export OTEL_SERVICE_NAME="nodejs-example"
 export OTEL_TRACES_EXPORTER="jaeger-thrift-splunk"
 export OTEL_EXPORTER_JAEGER_ENDPOINT="https://ingest.${REALM}.signalfx.com/v2/trace"
-# export SPLUNK_ACCESS_TOKEN="<enter here>"
+export SPLUNK_ACCESS_TOKEN="<enter token here>"
 export OTEL_RESOURCE_ATTRIBUTES='deployment.environment=nodejs-example'
 
-node nodetest.js
+node node_example.js
