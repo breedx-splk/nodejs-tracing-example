12ccf37c3a27b73867b2c7d07e25436a7a9604cb
Jason Plumb <jplumb@splunk.com>
2025-08-14 08:23:47 -0700

    blocking work instead of timeout

--- express_hello.js
+++ express_hello.js
@@ -7,15 +7,15 @@ const tracer = trace.getTracer('example-tracer');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  setTimeout(() => { 
-    tracer.startActiveSpan('outer', child => {
+  const start = Date.now();
+  while (Date.now() - start < 1000) {}
+  tracer.startActiveSpan('outer', child => {
       console.log('  a child...');
-      setTimeout(() => { 
-        res.end('Hi, you silly goose!\n');
-        child.end();
-      }, 1000);
-    });
-  }, 1000);
+      const start = Date.now();
+      while (Date.now() - start < 1000) {}
+      res.end('Hi, you silly goose!\n');
+      child.end();
+  });
 });
 
 const port = 3000;
